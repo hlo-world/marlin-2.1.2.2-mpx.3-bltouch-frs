cf1fcd8a1baa436e3b42425010a78016a49e5c62
hlo-world <9633055+hlo-world@users.noreply.github.com>
2024-06-07 00:29:04 -0400

    Increased Z_CURRENT to 1000 mA for dual Z motors

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2771,7 +2771,7 @@
   #endif
 
   #if AXIS_IS_TMC_CONFIG(Z)
-    #define Z_CURRENT       800
+    #define Z_CURRENT       1000
     #define Z_CURRENT_HOME  Z_CURRENT
     #define Z_MICROSTEPS     16
     #define Z_RSENSE          0.11
