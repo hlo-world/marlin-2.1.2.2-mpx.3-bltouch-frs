adf5d9d8a7d94dd812d1c42df9b8576d11c2779d
hlo-world <9633055+hlo-world@users.noreply.github.com>
2024-06-24 17:18:00 -0400

    Adjust PETG Preheat temp config

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2299,7 +2299,7 @@
 #define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
 
 #define PREHEAT_2_LABEL       "PETG"
-#define PREHEAT_2_TEMP_HOTEND 190
+#define PREHEAT_2_TEMP_HOTEND 210
 #define PREHEAT_2_TEMP_BED     90
 #define PREHEAT_2_TEMP_CHAMBER  0
 #define PREHEAT_2_FAN_SPEED     0 // Value from 0 to 255
